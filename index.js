28154529d1b2eb80954fbdb9e6dc3871bfb972f2
six-standard <dbrrl1127@gmail.com>
2024-08-06 19:56:09 +0900

    add

--- index.js
+++ index.js
@@ -2,12 +2,37 @@ const timer = document.querySelector("#timer");
 const audio = document.querySelector("audio");
 const video = document.querySelector("video");
 const button = document.querySelector("#sound");
+const startButton = document.querySelector("#startButton");
+
+const speedMinus = document.querySelector("#speedMinus");
+const speedCount = document.querySelector("#speedCount");
+const speedPlus = document.querySelector("#speedPlus");
 
 let time = 0;
 audio.muted = false;
 
-// video.playbackRate = 0.75;
+let speed = 1.0;
+video.playbackRate = speed;
 audio.volume = 0.3;
+
+speedCount.innerHTML = video.playbackRate;
+
+speedMinus.addEventListener("click", () => {
+  if (speed > 0.1) speed -= 0.05;
+  video.playbackRate = speed;
+  speedCount.innerHTML = speed.toFixed(2);
+});
+
+speedPlus.addEventListener("click", () => {
+  if (speed < 10.0) speed += 0.05;
+  video.playbackRate = speed;
+  speedCount.innerHTML = speed.toFixed(2);
+});
+
+startButton.addEventListener("click", () => {
+  startButton.style.display = "none";
+  audio.play();
+});
 
 button.addEventListener("click", () => {
   button.innerHTML = `소리 ${audio.muted ? "켬" : "끔"}`;
@@ -18,5 +43,5 @@ const interval = setInterval(() => {
   time++;
   timer.innerHTML = `${(time / 3600).toFixed(0)}시간 ${(time / 60).toFixed(
     0
-  )}분 ${time % 60}초째 보고 있다`;
+  )}분 ${time % 60}초`;
 }, 1000);
